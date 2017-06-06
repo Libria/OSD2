d63da27741568ba77a5bbba83a1dc5e2eb998e6a
Libria <litaparit732@hanyang.ac.kr>
2017-06-06 16:00:36 +0900

    Add line delete and Move

--- Project#2/Tetris.py
+++ Project#2/Tetris.py
@@ -2,6 +2,9 @@ from random import *
 import pygame
 from pygame.locals import *
 import time
+import sys
+
+pygame.init()
 
 pygame.display.set_caption('OSD2 Tetrix')
 
@@ -16,33 +19,123 @@ speed = 1
 # 블럭들
 tetrominoes = [0, 0, 0, 0, 0, 0, 0]
 # I : 막대, cyan 컬러
-tetrominoes[0]=[[[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],[[0,1,0,0],[0,1,0,0],[0,1,0,0],[0,1,0,0]]]
+tetrominoes[0]=[[
+                [1,1,1,1]],
+
+                [
+                [0,1,0,0],
+                [0,1,0,0],
+                [0,1,0,0],
+                [0,1,0,0]]]
 #colors[0]=0x00FFFF
 # T : ㅗ, purple 컬러
-tetrominoes[1]=[[[0,0,0,0],[1,1,1,0],[0,1,0,0],[0,0,0,0]],[[0,1,0,0],[1,1,0,0],[0,1,0,0],[0,0,0,0]],[[0,1,0,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]],[[0,1,0,0],[0,1,1,0],[0,1,0,0],[0,0,0,0]]]
+tetrominoes[1]=[[
+                [1,1,1,0],
+                [0,1,0,0]],
+
+                [
+                [0,1,0,0],
+                [1,1,0,0],
+                [0,1,0,0]],
+
+                [
+                [0,1,0,0],
+                [1,1,1,0]],
+
+                [
+                [0,1,0,0],
+                [0,1,1,0],
+                [0,1,0,0]]]
 #colors[1]=0x767676
 # L : ㄱ회전, orange 컬러
-tetrominoes[2]=[[[0,0,0,0],[1,1,1,0],[1,0,0,0],[0,0,0,0]],[[1,1,0,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],[[0,0,1,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]],[[0,1,0,0],[0,1,0,0],[0,1,1,0],[0,0,0,0]]]
+tetrominoes[2]=[[
+                [1,1,1,0],
+                [1,0,0,0]],
+
+                [
+                [1,1,0,0],
+                [0,1,0,0],
+                [0,1,0,0]],
+
+                [
+                [0,0,1,0],
+                [1,1,1,0]],
+
+                [
+                [0,1,0,0],
+                [0,1,0,0],
+                [0,1,1,0]]]
 #colors[2]=0xFFA500
 # J : ㄴ, blue 컬러
-tetrominoes[3]=[[[1,0,0,0],[1,1,1,0],[0,0,0,0],[0,0,0,0]],[[0,1,1,0],[0,1,0,0],[0,1,0,0],[0,0,0,0]],[[0,0,0,0],[1,1,1,0],[0,0,1,0],[0,0,0,0]],[[0,1,0,0],[0,1,0,0],[1,1,0,0],[0,0,0,0]]]
+tetrominoes[3]=[[
+                [1,0,0,0],
+                [1,1,1,0]],
+
+                [
+                [0,1,1,0],
+                [0,1,0,0],
+                [0,1,0,0]],
+
+                [
+                [1,1,1,0],
+                [0,0,1,0]],
+
+                [
+                [0,1,0,0],
+                [0,1,0,0],
+                [1,1,0,0]]]
 #colors[3]=0x0000FF
 # Z : z, red 컬러
-tetrominoes[4]=[[[0,0,0,0],[1,1,0,0],[0,1,1,0],[0,0,0,0]],[[0,0,1,0],[0,1,1,0],[0,1,0,0],[0,0,0,0]]]
+tetrominoes[4]=[[
+                [1,1,0,0],
+                [0,1,1,0]],
+
+                [
+                [0,0,1,0],
+                [0,1,1,0],
+                [0,1,0,0],
+                [0,0,0,0]]]
 #colors[4]=0xFF0000
 # S : 벼락, green 컬러
-tetrominoes[5]=[[[0,0,0,0],[0,1,1,0],[1,1,0,0],[0,0,0,0]],[[0,1,0,0],[0,1,1,0],[0,0,1,0],[0,0,0,0]]]
+tetrominoes[5]=[[
+                [0,1,1,0],
+                [1,1,0,0]],
+
+                [
+                [0,1,0,0],
+                [0,1,1,0],
+                [0,0,1,0]]]
 #colors[5]=0x00FF00
 # O : 네모, yellow 컬러
-tetrominoes[6]=[[[0,1,1,0],[0,1,1,0],[0,0,0,0],[0,0,0,0]]]
+tetrominoes[6]=[[
+                [0,1,1,0],
+                [0,1,1,0]]]
 #colors[6]=0xFFFF00
 
+def ColStart(blocknum, blockstate) :
+    start = 3
+    for row in range(len(tetrominoes[blocknum][blockstate])) :
+        for col in range(4) : 
+            if tetrominoes[blocknum][blockstate] == 1 :
+                if start > col : 
+                    start = col
+    return start
+
+
+def ColEnd(blocknum, blockstate) : 
+    end = 0
+    for row in range(len(tetrominoes[blocknum][blockstate])) :
+        for col in range(4) : 
+            if tetrominoes[blocknum][blockstate] == 1 :
+                if end < col : 
+                    end = col
+    return end
 
 def DrawBlock() :
     screen.lock()
     for col in range(cols) :
         for row in range(rows) :
-            if area[row][col] == 1 :
+            if area[row][col] >= 1 :
                 pygame.draw.rect(screen, (255,220,143), Rect((col*30,row*30), (27, 27)))
     pygame.display.update()
     screen.unlock()
@@ -54,105 +147,219 @@ def CleanUp() :
     screen.unlock()
 
 def InsertAreaBlock(num) :
-    chooseTet = tetrominoes[num]
+    tet = tetrominoes[num][0]
+    tetlen = len(tetrominoes[num][0])
+    row = 0
 
-    for tetcol in range(4) :
-        for tetrow in range(4) :
-            area[0 + tetrow][3 + tetcol] = chooseTet[0][tetrow][tetcol]
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[0 + row][3 + col] = area[0 + row][3 + col] + tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
 
 def DownBlock(blocklocation, blocknum, blockstate) :
-    def Check_1(list) :
-        for n in list :
-            if n == 1 :
-                return True
+    tet = tetrominoes[blocknum][blockstate]
+    tetlen = len(tet)
+    row = 0
+    x = blocklocation[0]
+    y = blocklocation[1]
 
-    block = tetrominoes[blocknum][blockstate]
-    srow = blocklocation[0]
-    scol = blocklocation[1]
-
-    alpha = 3
-    check1 = False
-    while not check1 :
-        check1 = Check_1(block[alpha])
-        alpha -= 1
-
-    if (srow + alpha + 2 >= rows) :
+    if (x + tetlen == 20) :
         return False
 
-    checkarea = False
-    targetarea = area[srow + alpha + 2]
+    for col in range(4) : 
+        if (x + tetlen < 20 and tet[tetlen - 1][col] > 0) :
+            if (area[x + tetlen][y + col] > 0) :
+                return False
 
-    for col in range(scol, scol + 4) :
-        if area[srow + alpha + 1][col] == 1 and targetarea[col] == 1 :
-            checkarea = True
-            break
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[x + row][y + col] = area[x + row][y + col] - tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
 
-    if checkarea == True :
-        return False
-    elif checkarea == False :
-        for row in range(srow, srow + 4) :
-            for col in range(scol, scol + 4) :
-                if area[row][col] == 1 :
-                    area[row][col] -= block[row - srow][col - scol]
-
-        for row in range(srow, srow + 3) :
-            for col in range(scol, scol + 4) :
-                area[row+1][col] += block[row - srow][col - scol]
-
-        return True
-
-
-
-def CheckHorizon(blocknum, blocklocation) :
-    def Check_1(list) :
-        for n in list :
-            if n == 1 :
-                return True
-
-    srow = blocklocation[0]
-    scol = blocklocation[1]
-    block = tetrominoes[blocknum][0]
-
-    for col in range(3,6) :
-        if area[4][col] == 1 :
-            return False
+    tetlen = len(tet)
+    row = 0
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[x + 1 + row][y + col] = area[x + 1 + row][y + col] + tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
 
     return True
 
+def CheckHorizon(blocknum, blocklocation) :
+    for col in range(10) :
+        for row in range(4) :
+            if (area[row][col] > 1) :
+                return False
 
-gameover = False
+    return True
 
-while not gameover :
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT :
-            gameover = True
+def Rotation(blocklocation, blocknum, blockstate) :
+    rotatelen = len(tetrominoes[blocknum])
+    x = blocklocation[0]
+    y = blocklocation[1]
 
-    speed_up = 1
-    blocknum = randint(0, 6)
-    noncollision = True
-    InsertAreaBlock(blocknum)
-    blocklocation = [0, 3]
-    blockstate = 0
+    blockstate2 = blockstate
+    if (blockstate2 + 1 == rotatelen) :
+        blockstate2 = 0
+    else :
+        blockstate2 += 1
 
-    if not CheckHorizon(blocknum, blocklocation) :
-        CleanUp()
-        DrawBlock()
-        noncollision = False
-        gameover = True
+    tet = tetrominoes[blocknum][blockstate]
+    tetlen = len(tet)
+    row = 0
 
-    while noncollision :
-        CleanUp()
-        DrawBlock()
-        if DownBlock(blocklocation, blocknum, blockstate) :
-            blocklocation[0] += 1
-            noncollision = True
+    while (tetlen > 0) :
+        for col in range(4) : 
+            area[x + row][y + col] = area[x + row][y + col] - tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
+
+    tet = tetrominoes[blocknum][blockstate2]
+    tetlen = len(tet)
+    row = 0
+
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[x + row][y + col] = area[x + row][y + col] + tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
+
+    return blockstate2
+
+def Move(blocklocation, blocknum, blockstate, way) :
+    rotatelen = len(tetrominoes[blocknum])
+    x = blocklocation[0]
+    y = blocklocation[1]
+
+    tet = tetrominoes[blocknum][blockstate]
+    tetlen = len(tet)
+    row = 0
+
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[x + row][y + col] = area[x + row][y + col] - tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
+
+    tet = tetrominoes[blocknum][blockstate]
+    tetlen = len(tet)
+    row = 0
+
+    while (tetlen > 0) :
+        for col in range(4) : 
+                area[x + row][y + col + way] = area[x + row][y + col + way] + tet[row][col]
+        tetlen = tetlen - 1
+        row = row + 1
+
+def Lineall() :
+    check = 0
+    row2 = 0
+
+    for row in range(20) :
+        for col in range(10) :
+            row2 = 19 - row
+            if (area[row2][col] == 1) :
+                check += 1
+            else :
+                break
+        if check == 10 :
+            return row2
         else :
-            noncollision = False
-        #time.sleep(float(0.1)/speed/speed_up)
-        time.sleep(0.1)
+            check = 0
 
-        for row in range(rows) :
-            print(area[row])
-        print("Cut")
+    return 0
+
+def DownAll(lineall) :
+    area2 = area
+
+    for row in range(0, lineall + 1) :
+        for col in range(10) :
+            area[row][col] = 0
+
+    for row in range(0, lineall + 1) :
+        for col in range(10) :
+            area[row + 1][col] = area2[row][col]
+
+def Run() : 
+    gameover = False
+    noncollision = False
+    while not gameover :
+        for event in pygame.event.get() :
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                sys.exit()
+
+            speed_up = 1
+
+            if (noncollision == False) :
+                blocknum = randint(0, 6)
+                noncollision = True
+                InsertAreaBlock(blocknum)
+                blocklocation = [0, 3]
+                blockstate = 0
+
+                if Lineall() != 0 :
+                    DownAll()
+
+            if not CheckHorizon(blocknum, blocklocation) :
+                noncollision = False
+                gameover = True
+                break
+
+            CleanUp()
+            DrawBlock()
+
+            if event.type == pygame.KEYDOWN :
+                if event.key == K_UP :
+                    blockstate = Rotation(blocklocation, blocknum, blockstate)
+                    CleanUp()
+                    DrawBlock()
+
+                elif event.key == K_RIGHT :
+                    colend = ColEnd(blocknum, blockstate)
+                    colstart = ColStart(blocknum, blockstate)
+                    if(blocklocation[1] + colend - colstart < 10) :
+                        temp = blockstate
+                        blockstate = Move(blocklocation, blocknum, blockstate, 1)
+                        blocklocation[1] += 1
+                        blockstate = temp
+                        CleanUp()
+                        DrawBlock()
+
+                elif event.key == K_LEFT :
+                    colstart = ColStart(blocknum, blockstate)
+                    if(blocklocation[1] - colstart > -1) :
+                        temp = blockstate
+                        blockstate = Move(blocklocation, blocknum, blockstate, -1)
+                        blocklocation[1] -= 1
+                        blockstate = temp
+                        CleanUp()
+                        DrawBlock()
+
+            downboolean = DownBlock(blocklocation, blocknum, blockstate)
+            if downboolean :
+                blocklocation[0] += 1
+            elif not downboolean :
+                noncollision = False
+
+            time.sleep(float(0.1)/speed/speed_up * 3)
+
+            '''downboolean = DownBlock(blocklocation, blocknum, blockstate)
+            if downboolean :
+                blocklocation[0] += 1
+            elif not downboolean :
+                noncollision = False
+
+            for row in range(rows) :
+                print(area[row])
+            print("Cut")'''
+
+            #if not hasattr(event, 'key') : 
+            #    continue
+
+Run()
 
 pygame.quit()
